56a21e4c98965441b415013eef889680b37c4091
Mark Wahnish <mark@abxygames.com>
2019-05-02 13:43:14 -0400

    Added transaction isolation for confluence

--- post-init.sh
+++ post-init.sh
@@ -2,4 +2,4 @@
 set -e
 sh /usr/bin/run-mysqld
 echo "${MYSQL_USER}"
-#mysql $mysql_flags -e "ALTER DATABASE sampledb CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+#mysql $mysql_flags -e "ALTER DATABASE sampledb CHARACTER SET utf8 COLLATE utf8_bin;"
